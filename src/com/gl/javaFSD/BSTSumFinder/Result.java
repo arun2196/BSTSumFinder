e1b8277fc5f113eb859c4619362655f0fed90cd8
arun2196 <arun.tiwari2196@gmail.com>
2023-02-23 16:04:09 +0530

    Update Result.java

--- src/com/gl/javaFSD/BSTSumFinder/Result.java
+++ src/com/gl/javaFSD/BSTSumFinder/Result.java
@@ -1,2 +1,21 @@
-package com.gl.javaFSD.BSTSumFinder;public class Result {
+package com.gl.javaFSD.BSTSumFinder;
+
+public class Result {
+  public static void main(String[] args)
+    {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(40);
+        tree.insert(20);
+        tree.insert(60);
+        tree.insert(10);
+        tree.insert(30);
+        tree.insert(50);
+        tree.insert(70);
+
+        boolean test
+                = tree.isPairPresent(tree.root, tree.root, 130);
+        if (!test)
+            System.out.println("Nodes are not found");
+    }
 }
