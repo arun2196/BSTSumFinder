b83ba35af29f0e95447469e3905615fe3c21a241
arun2196 <arun.tiwari2196@gmail.com>
2023-02-23 16:03:10 +0530

    Update BinarySearchTree.java

--- src/com/gl/javaFSD/BSTSumFinder/BinarySearchTree.java
+++ src/com/gl/javaFSD/BSTSumFinder/BinarySearchTree.java
@@ -1,2 +1,68 @@
-package com.gl.javaFSD.BSTSumFinder;public class BinarySearchTree {
+package com.gl.javaFSD.BSTSumFinder;
+class Node {
+
+    int data;
+    Node left, right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = null;
+    }
+}
+
+public class BinarySearchTree {
+   Node root;
+
+    BinarySearchTree() { root = null; }
+    void insert(int key) { root = insertRec(root, key); }
+
+    Node insertRec(Node root, int data)
+    {
+
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data)
+            root.left = insertRec(root.left, data);
+        else if (data > root.data)
+            root.right = insertRec(root.right, data);
+
+        return root;
+    }
+
+    boolean isPairPresent(Node root, Node temp, int target)
+    {
+        if (temp == null)
+            return false;
+
+        return search(root, temp, target - temp.data)
+                || isPairPresent(root, temp.left, target)
+                || isPairPresent(root, temp.right, target);
+    }
+
+    boolean search(Node root, Node temp, int k)
+    {
+
+        if (root == null)
+            return false;
+
+        Node c = root;
+        boolean flag = false;
+        while (c != null && flag != true) {
+            if (c.data == k && temp != c) {
+                flag = true;
+                System.out.println("Pair is (" + c.data + "," + temp.data + ")");
+                return true;
+            }
+            else if (k < c.data)
+                c = c.left;
+            else
+                c = c.right;
+        }
+
+        return false;
+    }
 }
